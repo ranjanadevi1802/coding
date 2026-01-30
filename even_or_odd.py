301c245dfe366c3ef2d39595a4015e746ca75b23
Ranjana Devi <ranjanadevi.180264@gmail.com>
2026-01-30 21:42:24 +0530

    First Commit

---
+++ even_or_odd.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isEven(self, n):
+        return n % 2 == 0
+
+# Example usage:
+solution = Solution()   
+print(solution.isEven(4))  # Output: True
+# Check if a number is even
+print(solution.isEven(5))  # Output: False
+
